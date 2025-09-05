2d4e405b81323cbd088b98a6f25324df92f55373
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2025-09-05 14:53:40 -0500

    Update ejercicio1.py

--- Semana 1/ejercicio1.py
+++ Semana 1/ejercicio1.py
@@ -1,1 +1,8 @@
-print('Hello')
+n = 8
+if n % 2: #n % 2 = 0 
+  print("El número es impar") 
+else: 
+  print("El número es par") 
+
+#True (1), 0 -> False
+#No ingresa al if, porque el resultado es 0 y eso se interpreta como falso
