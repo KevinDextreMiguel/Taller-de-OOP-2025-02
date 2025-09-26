c285e65cd1ecfe36d4ca3b46c302960f7da39063
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2025-09-26 18:12:38 -0500

    Create ejercicio2.py

---
+++ Semana 5/ejercicio2.py
@@ -0,0 +1,25 @@
+import math
+ 
+class Cono:
+  def __init__(self,radio_mayor,radio_menor,altura):
+    self.radio_mayor = radio_mayor
+    self.radio_menor = radio_menor
+    self.altura = altura
+  
+  def calcular_generatriz(self):
+    generatriz = (self.altura ** 2 + (self.radio_mayor - self.radio_menor)) ** 0.5
+    return generatriz
+  
+  def calcular_area(self):
+    area = math.pi * (self.radio_mayor ** 2 + self.radio_menor ** 2 + self.calcular_generatriz() * (self.radio_mayor + self.radio_menor))
+    print(f"El àrea es: {area}")
+
+  def calcular_volumen(self):
+    volumen = math.pi * self.altura / 3 * (self.radio_mayor ** 2 + self.radio_menor ** 2 + self.radio_mayor * self.radio_menor)
+    print(f"El volumen es: {volumen}")
+
+#Craer Objeto
+objeto_cono1 = Cono(8,3,10)
+objeto_cono1.calcular_generatriz()
+objeto_cono1.calcular_area()
+objeto_cono1.calcular_volumen()
